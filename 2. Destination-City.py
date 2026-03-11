1a4524aff6ab72ef07d7801d2b9ef387dac87785
Jay Mirasol <jaymirasol133@gmail.com>
2026-03-11 20:33:26 +0800

    Renamed variables for better readability

--- 2. Destination-City.py
+++ 2. Destination-City.py
@@ -9,14 +9,14 @@
 import test
 
 def Solution(paths: list[list[str]]) -> str:
-    outs = set()
-    ins = set()
+    outgoingCity = set()
+    incomingCity = set()
 
     for a,b in paths:
-        outs.add(a)
-        ins.add(b)
+        outgoingCity.add(a)
+        incomingCity.add(b)
    
-    return [b for b in ins if b not in outs][0]
+    return [b for b in incomingCity if b not in outgoingCity][0]
 
 #Testing
 test_cases = [
